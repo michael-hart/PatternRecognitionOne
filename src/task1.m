318a7e66af73fc93f9d210f5dbc4145680d57235
Michael Hart <michael.hart1994@gmail.com>
2016-12-08 21:20:54 +0000

    Added average face and PCA of both kinds, with issues

--- src/task1.m
+++ src/task1.m
@@ -9,7 +9,32 @@ disp(['Training length is ' num2str(size(training, 2)) ...
 % Find average face
 N = size(training, 2);
 average_face = sum(training, 2) ./ N;
-show_face(average_face);
+% show_face(average_face);
 
-% Path to custom libsvm location on windows
-addpath('D:/Git/libsvm/windows');
+% Remove the average face from the training set
+D = size(training, 1);
+% This is a trick for removing face from all columns at once
+A = training - average_face(:, ones(1,N));
+
+% Compute covariance matrix; First we're using S = (1/N) A A(T)
+S = (1/N) * A * A';
+
+% Calculate the eigenvectors of this matrix
+[S_eig_vec, S_eig_val] = eig(S);
+
+% Eigenvalues are aleady in descending order!
+disp(S_eig_val(1:10, 1:10));
+% disp(S_eig_val(2566:2576, 2566:2576));
+disp(S_eig_vec(1:10, 1:10));
+
+% We want to work out how many non-zero eigenvalues there are
+% size(S_eig_val); TODO Meng why are there 2576 nonzero?!
+nonzero = nnz(S_eig_val);
+disp(['Number of nonzero elements is ' num2str(nonzero)]);
+
+% Repeat the above process for S = (1/N) A(T) A
+S2 = (1/N) * A' * A;
+[S2_eig_vec, S2_eig_val] = eig(S2);
+nonzero2 = nnz(S_eig_val);
+
+% Now compare 1:M of each matrix TODO
