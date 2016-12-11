da8d141b89ca74a6e555bd897270e20e183558d1
Michael Hart <michael.hart1994@gmail.com>
2016-12-11 20:00:32 +0000

    Flattened list structure to allow 1v1. WARNING: Takes 2h40 to compute entire script.

--- src/generate_one_vs_one.m
+++ src/generate_one_vs_one.m
@@ -7,36 +7,31 @@ load(strjoin({res_path 'partitioned.mat'}, filesep));
 
 % Figure out how many classes there are in the data, M
 M = max(l_train);
-M = 3;
 
 % For now, forget preallocation, because it's so fast compared to SVM
 % training
 
+clear one_vs_one_svm
+
+% Preallocate for efficiency
+correct = get_class(l_train, training, M);
+incorrect = get_class(l_train, training, M-1);
+one_vs_one_svm(M*(M-1)) = svmtrain(correct, incorrect);
+current = 1;
+
 for class=1:M
-    clear one_vs_one
-    correct = get_class(l_train, training, class);
-    if class < M
-        incorrect = get_class(l_train, training, M);
-    else
-        incorrect = get_class(l_train, training, M-1);
-    end
-    one_vs_one(M-1) = svmtrain(correct, incorrect);
-    current = 1;
-    for foe=1:M-1
-        if class==foe
-            continue
-        end
-        if class==M && foe == M-1
-            % Already calculated
+    for foe=1:M
+        if class==foe || current == M*(M-1)
+            % Either it's the same class or it's already been done
             continue
         end
         correct = get_class(l_train, training, class);
         incorrect = get_class(l_train, training, foe);
-        one_vs_one(current) = svmtrain(correct, incorrect);
+        one_vs_one_svm(current) = svmtrain(correct, incorrect);
         current = current + 1;
     end
-    one_vs_one_svm(class) = one_vs_one;
 end
 
 % Save calculated SVM list to prevent recalculating!
-save(strjoin({res_path 'one_vs_one.mat'}, filesep), 'one_vs_one_svm');
+save(strjoin({res_path 'one_vs_one.mat'}, filesep), 'one_vs_one_svm', ...
+                                                    '-v7.3');
