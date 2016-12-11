415f467660b08fd3ed22e8816ee8b443a39f859a
Michael Hart <michael.hart1994@gmail.com>
2016-12-11 23:00:36 +0000

    Changed to use labels and training data instead of training/rest

--- src/generate_one_vs_rest_svm.m
+++ src/generate_one_vs_rest_svm.m
@@ -2,20 +2,32 @@
 addpath('D:\Git\libsvm\windows');
 
 % First step is to load the data
+clear
 res_path = get_res_path();
 load(strjoin({res_path 'partitioned.mat'}, filesep));
 
 % Figure out how many classes there are in the data, M
 M = max(l_train);
+N = size(l_train, 2);
 
 % Train LAST class to preallocate memory
-[ current, rest ] = get_class_and_rest(l_train, training, M);
-one_vs_rest_svm(M) = svmtrain(current, rest);
+svm_labels = ones(N, 1);
+for j=1:N
+    if l_train(j) ~= M
+        svm_labels(j) = -1;
+    end
+end
+one_vs_rest_svm(M) = svmtrain(svm_labels, training');
 
-% Iterate over rest of classes and train an SVM of 1 vs rest for each
+% Train remaining classes using the same method
 for i=1:M-1
-    [ current, rest ] = get_class_and_rest(l_train, training, i);
-    one_vs_rest_svm(i) = svmtrain(current, rest);
+    svm_labels = ones(N, 1);
+    for j=1:N
+        if l_train(j) ~= i
+            svm_labels(j) = -1;
+        end
+    end
+    one_vs_rest_svm(i) = svmtrain(svm_labels, training');
 end
 
 % Save calculated SVM list to prevent recalculating!
